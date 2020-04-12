25e93bf8916f3b28f963b1dce1c3fd66daa65232
szczepaniakmichal <mssk87@gmail.com>
2020-04-13 01:52:45 +0200

    second method to find element

--- src/App.js
+++ src/App.js
@@ -6,31 +6,33 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            person: [{"id":1,"name":"Drucie"},
-                {"id":2,"name":"Mabelle"},
-                {"id":3,"name":"Alyce"},
-                {"id":4,"name":"Vannie"},
-                {"id":5,"name":"Beale"},
-                {"id":6,"name":"Mikaela"},
-                {"id":7,"name":"Meyer"},
-                {"id":8,"name":"Angelico"},
-                {"id":9,"name":"Brina"},
-                {"id":10,"name":"Elise"},
-                {"id":11,"name":"Karlik"},
-                {"id":12,"name":"Christiano"},
-                {"id":13,"name":"Ronald"},
-                {"id":14,"name":"Rolland"},
-                {"id":15,"name":"Paulita"},
-                {"id":16,"name":"Tally"},
-                {"id":17,"name":"Johnette"},
-                {"id":18,"name":"Evonne"},
-                {"id":19,"name":"Myrtle"},
-                {"id":20,"name":"Guillermo"}]
+            person: [{"id": 1, "name": "Drucie"},
+                {"id": 2, "name": "Mabelle"},
+                {"id": 3, "name": "Alyce"},
+                {"id": 4, "name": "Vannie"},
+                {"id": 5, "name": "Beale"},
+                {"id": 6, "name": "Mikaela"},
+                {"id": 7, "name": "Meyer"},
+                {"id": 8, "name": "Angelico"},
+                {"id": 9, "name": "Brina"},
+                {"id": 10, "name": "Elise"},
+                {"id": 11, "name": "Karlik"},
+                {"id": 12, "name": "Christiano"},
+                {"id": 13, "name": "Ronald"},
+                {"id": 14, "name": "Rolland"},
+                {"id": 15, "name": "Paulita"},
+                {"id": 16, "name": "Tally"},
+                {"id": 17, "name": "Johnette"},
+                {"id": 18, "name": "Evonne"},
+                {"id": 19, "name": "Myrtle"},
+                {"id": 20, "name": "Guillermo"}]
         };
     }
 
-    handleRemoveClick = (e) => {
-        const person = [...this.state.person].filter(person => person.id !== e);
+    handleRemoveClick = (id) => {
+        const person = [...this.state.person];
+        const index = person.findIndex(person => person.id === id);
+        person.splice(index, 1);
         this.setState({
             person,
         })
